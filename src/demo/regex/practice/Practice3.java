dcf01db25e3332344b6027e9f1ec25ae847cea97
jun <jjwjun10@gmail.com>
2021-03-10 23:57:54 +0900

    failure

--- src/demo/regex/practice/Practice3.java
+++ src/demo/regex/practice/Practice3.java
@@ -6,6 +6,8 @@ public class Practice3 {
     public static void main(String args[]){
         String rex = "\\w";
         Pattern pattern = Pattern.compile(rex);
-        boolean result = pattern.matcher("t").matches().predicate();
+
+//        boolean result = pattern.matcher("t").matches().predicate();
+
     }
 }
